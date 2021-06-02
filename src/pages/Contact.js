3c782c4a10940a6a88035becae9937d0010a451b
Christian Pelayo <chris.pelayo.dev@gmail.com>
2021-06-02 14:40:34 -0700

    Messing with the photos.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ export const Contact = ({ formValues, setFormValues, onInputChange, onFormSubmit
         <div className="flex flex-col justify-center items-center min-h-nav-screen-fun">
             <h2 className="text-center pb-8 text-2xl px-4">Get in contact with me!<br /> Lets {colorChanger('create')} something.</h2>
             <div className="flex flex-col justify-center items-center md:flex-row md:pb-10">
-                <img className="w-60" src={process.env.PUBLIC_URL + (fun ? './imgs/contact_guy_fun.png' : './imgs/contact_guy.png')} alt="contact_guy" />
+                <img className="w-60" src={fun ? process.env.PUBLIC_URL + './imgs/contact_guy_fun.png' : process.env.PUBLIC_URL + './imgs/contact_guy.png'} alt="contact_guy" />
                 <EmailForm 
                     formValues={formValues}
                     setFormValues={setFormValues}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export const Home = ({ fun, setFun, colorChanger }) => {
         <div className="flex flex-col justify-center items-center min-h-nav-screen-fun">
             <div className="-mt-16">
                 <div className="flex items-end">
-                    <img className="w-24 md:w-36 lg:w-60" src={fun ? process.env.PUBLIC_URL + "./imgs/guy_head_fun_cropped.png" : process.env.PUBLIC_URL + "./imgs/guy_head_cropped.png"} alt="guy" />
+                    <img className="w-24 md:w-36 lg:w-60" src={fun ? "./imgs/guy_head_fun_cropped.png" : "./imgs/guy_head_cropped.png"} alt="guy" />
                     <img className="h-8 md:h-10 lg:h-20 welcome" src="./imgs/guy_hand.png" alt="guy_hand" />
                 </div>
                 <h1 className="text-2xl md:text-4xl lg:text-6xl">{colorChanger('Welcome!')}</h1>
